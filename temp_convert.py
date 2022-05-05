df45109c1048a6da9fb1c453310ac93afa46c502
nictheriscer <nicolas.riscalas@stu.ocsb.ca>
2022-05-05 20:42:11 +0000

    Fkdsjalkfj

--- temp_convert.py
+++ temp_convert.py
@@ -5,17 +5,21 @@
 # display 5 ints in a line
 
 
-def main():
+def fahrenheit():
     while True:
         temp_celc_str = input("Enter the temperature in celcius: ")
         try:
             temp_celc = float(temp_celc_str)
-            temp_feren = ((9 / 5) * temp_celc) + 32
-            print("the temperature in farenheit is {}".format(temp_feren))
+            temp_fahren = ((9 / 5) * temp_celc) + 32
+            print("the temperature in farenheit is {}".format(temp_fahren))
         except:
             print("You have to enter a real number")
         input("To run the program again just press <enter>")
     return 0
+
+
+def main():
+    fahrenheit()
 
 
 if __name__ == "__main__":
